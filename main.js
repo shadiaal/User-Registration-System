2a89a2e2d0b85b2e7ffd4b60932adcbb333219ae
shadia <162451775+shadiaal@users.noreply.github.com>
2025-02-22 14:18:29 +0300

    Delete main.js

--- main.js
+++
@@ -1,7 +0,0 @@
-import { Form } from "./components/Form.js";
-document.addEventListener("DOMContentLoaded", () => {
-    const app = document.getElementById("app");
-    if (app) {
-        new Form().render(app);
-    }
-});
